7eebd4c4859ba40f22fbf41a3812533cd3e02885
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-10-18 13:37:43 +0100

    working

--- environment/provision.sh
+++ environment/provision.sh
@@ -10,3 +10,10 @@ sudo apt-get install python-software-properties -y
 wget -qO- https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g pm2
+
+cd /home/vagrant/app/app
+pm2 start app.js
+
+sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+sudo ln -s /home/vagrant/app/app/default /etc/nginx/sites-available/default
+sudo service nginx restart
